a94f941940823596d72159b017a65aebb8e4fdd7
marsic3 <arsic.milos30@gmail.com>
2023-08-14 22:19:31 +0200

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="icon" href="/favicon.ico" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
                     <link href="https://fonts.googleapis.com/css2?family=Bungee&family=Source+Code+Pro:ital,wght@0,400;0,500;0,600;0,700;1,500;1,600&family=Source+Sans+3&display=swap" rel="stylesheet" />
                 </Head>
